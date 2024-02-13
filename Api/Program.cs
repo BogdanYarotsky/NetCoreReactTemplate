d25bf57be3cf074562badb0c419a43e78fa71555
BogdanYarotsky <byarotsky@gmail.com>
2024-02-13 12:01:32 +0100

    added url to react app

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,20 +1,12 @@
 namespace Api;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
-
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-
-        app.UseAuthorization();
 
         var summaries = new[]
         {
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [forecast, setForecast] = useState(undefined);
+
+  const fetchForecast = async () => {
+    try {
+      const response = await fetch(`${apiEndpoint}/weatherforecast`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setForecast(data);
+    } catch (error) {
+      console.error('Failed to fetch forecast:', error);
+    }
+  };
 
   return (
     <>
@@ -18,18 +33,24 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={fetchForecast}>
+          Fetch Forecast
         </button>
+        {forecast && (
+          <div>
+            <h2>Forecast</h2>
+            <pre>{JSON.stringify(forecast)}</pre>
+          </div>
+        )}
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR updates.
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
